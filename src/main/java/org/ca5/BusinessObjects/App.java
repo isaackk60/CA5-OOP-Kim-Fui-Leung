79d1d8686c3071c4ac4774a243c0613623d0472b
Kim Fui Leung <93153597+isaackk60@users.noreply.github.com>
2024-03-16 18:49:21 +0000

    adjust menu

--- src/main/java/org/ca5/BusinessObjects/App.java
+++ src/main/java/org/ca5/BusinessObjects/App.java
@@ -12,13 +12,6 @@ import java.util.Scanner;
 
 public class App {
 
-    /**
-
-     * Author:  Jamie Duffy Creagh
-     * Other contributors:  Kim Fui Leung, Aoife Murphy
-     * Date: 8-03-24
-
-     */
     public static void main(String[] args) throws DaoException {
 
         BookDaoInterface IBookDao = new MySqlBooks();
@@ -29,78 +22,173 @@ public class App {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("\nMenu:");
-            System.out.println("1. View Database");
-            System.out.println("2. Add to Database");
-            System.out.println("3. Delete from Database");
-            System.out.println("4. Get Book from Database Using ID");
-            System.out.println("5. Update book from Database Using ID");
-            System.out.println("6. Filter book from Database");
-            System.out.println("0. Exit");
 
-            System.out.print("Enter your choice: ");
-            int choice = scanner.nextInt();
+            System.out.println("====================================================");
+            System.out.println("                       Menu:                        ");
+            System.out.println("====================================================");
+            System.out.println("=            1. View Database                      =");
+            System.out.println("=            2. Add to Database                    =");
+            System.out.println("=            3. Delete from Database               =");
+            System.out.println("=            4. Get Book from Database Using ID    =");
+            System.out.println("=            5. Update Book                        =");
+            System.out.println("=            6. Filter Book                        =");
+            System.out.println("=            0. Exit                               =");
+            System.out.println("====================================================");
 
-            switch (choice) {
-                case 1:
-                    System.out.println(IBookDao.getAllBooks());
-                    break;
-                case 2:
-                    IBookDao.insertBook(scanner);
-                    break;
-                case 3:
-                    System.out.println("Enter the ID of the book to delete:");
-                    int deleteId = scanner.nextInt();
-                    IBookDao.deleteBookById(deleteId);
-                    break;
-                case 4:
-                    System.out.println("Enter the ID of the book to display details:");
-                    int bookId = scanner.nextInt();
 
-                    Book book = IBookDao.getBookById(bookId);
-                    if (book != null) {
-                        System.out.println(book.toString());
-                    } else {
-                        System.out.println("No Book found with the id " + bookId);
+            int choice = -1;
+            while (choice < 0) {
+                System.out.println("Enter your choice:");
+                if (scanner.hasNextInt()) {
+                    choice = scanner.nextInt();
+                    if (choice < 0) {
+                        System.out.println("Invalid Choice. Please enter a positive integer.");
                     }
+                } else {
+                    System.out.println("Invalid choice. Please enter a valid integer for choice.");
+                    scanner.next(); // Clear the invalid input
+                }
 
-                    break;
-                case 5:
-                    System.out.println("Enter the ID of the book to update details:");
-                    int id = scanner.nextInt();
-                    scanner.nextLine();
+                switch (choice) {
+                    case 1:
+                        System.out.println(IBookDao.getAllBooks());
+                        break;
+                    case 2:
+                        IBookDao.insertBook(scanner);
+                        break;
+                    case 3:
+                        System.out.println("Enter the ID of the book to delete:");
+                        int deleteId = scanner.nextInt();
+                        IBookDao.deleteBookById(deleteId);
+                        break;
+                    case 4:
+                        System.out.println("Enter the ID of the book to display details:");
+                        int bookId = scanner.nextInt();
 
-                    // Prompt user for updated book details
-                    System.out.println("Enter new book title:");
-                    String title = scanner.nextLine();
+                        Book book = IBookDao.getBookById(bookId);
+                        if (book != null) {
+                            System.out.println(book.toString());
+                        } else {
+                            System.out.println("No Book found with the id " + bookId);
+                        }
 
-                    System.out.println("Enter new book genre:");
-                    String genre = scanner.nextLine();
+                        break;
 
-                    System.out.println("Enter new book author:");
-                    String author = scanner.nextLine();
+                    case 5:
+                        System.out.println("Enter the ID of the book to update details:");
+                        int id = 0;
+                        while (id < 1) {
+                            System.out.println("Enter the ID of the book to update details:");
+                            if (scanner.hasNextInt()) {
+                                id = scanner.nextInt();
+                                if (id < 1) {
+                                    System.out.println("Invalid ID. Please enter a positive integer.");
+                                }
+                            } else {
+                                System.out.println("Invalid input. Please enter a valid integer ID.");
+                                scanner.next(); // Clear the invalid input
+                            }
+                        }
 
-                    System.out.println("Enter new number of pages:");
-                    int pages = scanner.nextInt();
 
-                    System.out.println("Is the book part of a series? (true/false):");
-                    boolean series = scanner.nextBoolean();
+                        scanner.nextLine();
 
-                    System.out.println("Enter new stock quantity:");
-                    int stock = scanner.nextInt();
+                        String title = "";
+                        while (title.trim().isEmpty()) {
+                            System.out.println("Enter new book title:");
+                            title = scanner.nextLine().trim();
+                            if (title.trim().isEmpty()) {
+                                System.out.println("Invalid input. Please try again.");
+                            }
+                        }
 
-                    System.out.println("Enter new book rating:");
-                    double rating = scanner.nextDouble();
+                        // Prompt for book genre
+                        String genre = "";
+                        while (genre.trim().isEmpty()) {
+                            System.out.println("Enter new book genre:");
+                            genre = scanner.nextLine().trim();
+                            if (genre.trim().isEmpty()) {
+                                System.out.println("Invalid input. Please try again.");
+                            }
+                        }
 
-                    scanner.nextLine();
+                        // Prompt for book author
+                        String author = "";
+                        while (author.trim().isEmpty()) {
+                            System.out.println("Enter new book author:");
+                            author = scanner.nextLine().trim();
+                            if (author.trim().isEmpty()) {
+                                System.out.println("Invalid input. Please try again.");
+                            }
+                        }
 
-                    System.out.println("Enter new book description:");
-                    String description = scanner.nextLine();
+                        // Prompt for number of pages
+                        int pages = 0;
+                        while (pages <= 0) {
+                            System.out.println("Enter number of pages:");
+                            if (scanner.hasNextInt()) {
+                                pages = scanner.nextInt();
+                            } else {
+                                System.out.println("Invalid input. Please enter a valid number.");
+                                scanner.next();
+                            }
+                        }
 
-                    System.out.println("Enter new book publisher:");
-                    String publisher = scanner.nextLine();
+                        // Prompt for whether the book is part of a series
+                        boolean series = false;
+                        System.out.println("Is the book part of a series? (true/false):");
+                        while (!scanner.hasNextBoolean()) {
+                            System.out.println("Invalid input. Please enter true or false.");
+                            scanner.next(); // clear the invalid input
+                        }
+                        series = scanner.nextBoolean();
 
-                    try {
+                        // Prompt for stock quantity
+                        int stock = 0;
+                        while (stock < 0) {
+                            System.out.println("Enter stock quantity:");
+                            if (scanner.hasNextInt()) {
+                                stock = scanner.nextInt();
+                            } else {
+                                System.out.println("Invalid input. Please enter a valid number.");
+                                scanner.next(); // clear the invalid input
+                            }
+                        }
+
+                        // Prompt for book rating
+
+                        double rating = 0.0;
+                        while (rating < 0) {
+                            System.out.println("Enter book rating:");
+                            if (scanner.hasNextDouble()) {
+                                rating = scanner.nextDouble();
+                            } else {
+                                System.out.println("Invalid input. Please enter a valid number.");
+                                scanner.next(); // clear the invalid input
+                            }
+                        }
+
+
+                        // Prompt for book description
+                        String description = "";
+                        while (description.trim().isEmpty()) {
+                            System.out.println("Enter new book description:");
+                            description = scanner.nextLine().trim();
+                            if (description.trim().isEmpty()) {
+                                System.out.println("Invalid input. Please try again.");
+                            }
+                        }
+
+                        // Prompt for book publisher
+                        String publisher = "";
+                        while (publisher.trim().isEmpty()) {
+                            System.out.println("Enter new book publisher:");
+                            publisher = scanner.nextLine().trim();
+                            if (publisher.trim().isEmpty()) {
+                                System.out.println("Invalid input. Please try again.");
+                            }
+                        }
+
                         Book updatedBook = new Book(id, title, genre, author, pages, series, stock, rating, description,
                                 publisher);
                         Book b = IBookDao.updateBook(id, updatedBook);
@@ -110,49 +198,52 @@ public class App {
                         } else {
                             System.out.println("No Book found with the id " + id);
                         }
-                    } catch (DaoException e) {
-                        System.out.println("Error updating book: " + e.getMessage());
-                    }
-                    break;
-                case 6:
-                    System.out.println("\nMenu:");
-                    System.out.println("1. Filter by page over 400");
-                    System.out.println("2. Filter by page below 400");
-                    System.out.print("Enter your choice: ");
-                    int filterChoice=scanner.nextInt();
-                    switch (filterChoice){
-                        case 1:
-                            List<Book> booksOver400Pages = IBookDao.findBooksUsingFilter(new BookPageComparatorOver400());
-                            if(!booksOver400Pages.isEmpty()){
-                                for(Book bookFilter:booksOver400Pages){
-                                    System.out.println(bookFilter.toString());
+                        break;
+
+
+                    case 6:
+                        System.out.println("\nMenu:");
+                        System.out.println("====================================================");
+                        System.out.println("=           1. Filter by page over 400              ");
+                        System.out.println("=           2. Filter by page below 400             ");
+                        System.out.println("====================================================");
+                        System.out.print("Enter your choice: ");
+                        int filterChoice = scanner.nextInt();
+                        switch (filterChoice) {
+                            case 1:
+                                List<Book> booksOver400Pages = IBookDao.findBooksUsingFilter(new BookPageComparatorOver400());
+                                if (!booksOver400Pages.isEmpty()) {
+                                    for (Book bookFilter : booksOver400Pages) {
+                                        System.out.println(bookFilter.toString());
+                                    }
+                                } else {
+                                    System.out.println("No book found with page over 400");
                                 }
-                            }else{
-                                System.out.println("No book found with page over 400");
-                            }
-                            break;
-                        case 2:
-                            List<Book> booksUnder400Pages = IBookDao.findBooksUsingFilter(new BookPageComparatorUnder400());
-                            if(!booksUnder400Pages.isEmpty()){
-                                for(Book bookFilter:booksUnder400Pages){
-                                    System.out.println(bookFilter.toString());
+                                break;
+                            case 2:
+                                List<Book> booksUnder400Pages = IBookDao.findBooksUsingFilter(new BookPageComparatorUnder400());
+                                if (!booksUnder400Pages.isEmpty()) {
+                                    for (Book bookFilter : booksUnder400Pages) {
+                                        System.out.println(bookFilter.toString());
+                                    }
+                                } else {
+                                    System.out.println("No book found with page below 400");
                                 }
-                            }else{
-                                System.out.println("No book found with page below 400");
-                            }
-                            break;
-                        default:
-                            System.out.println("Invalid choice. Please enter a valid option.");
-                    }
-                    break;
-                case 0:
-                    System.out.println("Exiting the program. Goodbye!");
-                    return;
-                default:
-                    System.out.println("Invalid choice. Please enter a valid option.");
+                                break;
+                            default:
+                                System.out.println("Invalid choice. Please enter a valid option.");
+                        }
+                        break;
+
+
+                    case 0:
+                        System.out.println("Exiting the program. Goodbye!");
+                        return;
+                    default:
+                        System.out.println("Invalid choice. Please enter a valid option.");
+                }
             }
+
         }
-
     }
-
 }
